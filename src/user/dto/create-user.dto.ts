c3b8fff85d6f0b8164e769ef240b40f3639b6fd5
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-11-04 18:50:04 +0400

    add Swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+import { IsEmail, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+  @MinLength(6, { message: 'Password must be at least 6 characters' })
+  password: string;
+}
